9c2fc184b6236f9b7ef69a16c96b9e8132d77786
stevetice <tice.steve@gmail.com>
2017-08-04 23:18:56 +0000

    Logged in users can submit product review

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,31 @@
+class ReviewsController < ApplicationController
+
+  def create
+
+    @product = Product.find params[:product_id]
+    @review = @product.reviews.new(review_params)
+    @review.user = current_user
+    if @review.save
+      redirect_to @product
+    else
+      redirect_to @product
+    end
+
+  end
+
+
+  def destroy
+
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(
+      :description,
+      :rating
+    )
+  end
+
+
+end
