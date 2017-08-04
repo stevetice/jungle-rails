f39f29d4099030b007002ddd37254343abe24461
stevetice <tice.steve@gmail.com>
2017-08-04 23:51:41 +0000

    Add before_action to prevent reviews_controller actions from being accesed by non-logged-in users.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,9 @@
 class ReviewsController < ApplicationController
 
+   before_action :set_review, only: :destroy
+   before_action :logged_in, only: [:create, :destroy]
+
+
   def create
 
     @product = Product.find params[:product_id]
@@ -15,17 +19,28 @@ class ReviewsController < ApplicationController
 
 
   def destroy
-
+    @review.destroy
+    redirect_to :back
   end
 
   private
 
-  def review_params
-    params.require(:review).permit(
-      :description,
-      :rating
-    )
-  end
+    def set_chicken
+      @review = Review.find(params[:id])
+    end
 
+    def review_params
+      params.require(:review).permit(
+        :description,
+        :rating
+      )
+    end
+
+    def logged_in
+      if !current_user
+        redirect_to :login
+    end
+
+  end
 
 end
